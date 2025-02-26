644a24f122ef7ee93b1fd7b4aab8d19a5e1a4cda
yacoquss <yacoquss@mail.ru>
2025-02-26 20:14:04 +0300

    some changes

---
+++ WebApplication1/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+        
+    }
+}
--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -7,21 +7,39 @@ namespace WebApplication1.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly ApplicationDbContext _context;
+    public HomeController(ILogger<HomeController> logger, 
+        ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
         return View();
     }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(User user)
+    {
+        _context.Users.Add(user);
+        _context.SaveChanges();
+        return RedirectToAction("Users");
+    }
 
     public IActionResult Privacy()
     {
         return View();
     }
+
+    public IActionResult Users()
+    {
+        IEnumerable<User> users = _context.Users;
+        return View(users);
+    }
+
+   
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
